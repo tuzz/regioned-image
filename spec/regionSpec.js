b850a668281da0def8b7f5735bfa11796d684406
Chris Patuzzo <chris@patuzzo.co.uk>
2014-12-14 15:34:06 +0000

    Implement Region#merge

--- spec/regionSpec.js
+++ spec/regionSpec.js
@@ -3,11 +3,10 @@
 var helpers = require("./specHelper");
 
 describe("Region", function () {
-  var whiteRegion;
-  var blackRegion;
+  var regionedImage, whiteRegion, blackRegion;
 
   beforeEach(function () {
-    var regionedImage = new RegionedImage("image.png");
+    regionedImage = new RegionedImage("image.png");
     helpers.triggerLoad();
 
     whiteRegion = regionedImage.buildRegion({ x: 0, y: 0 });
@@ -32,5 +31,38 @@ describe("Region", function () {
   it("has a 'boundaryColor' property", function () {
     expect(whiteRegion.boundaryColor).toEqual("#FFFFFF");
     expect(blackRegion.boundaryColor).toEqual("#000000");
+  });
+
+  describe("#contains", function () {
+    it("returns true if the coordinates are inside the region", function () {
+      expect(whiteRegion.contains({ x: 0, y: 0 })).toEqual(true);
+      expect(whiteRegion.contains({ x: 1, y: 1 })).toEqual(true);
+    });
+
+    it("returns false if the coordinates are outside the region", function () {
+      expect(whiteRegion.contains({ x: 3,  y: 5 })).toEqual(false);
+      expect(whiteRegion.contains({ x: 4,  y: 6 })).toEqual(false);
+      expect(whiteRegion.contains({ x: 0,  y: 9 })).toEqual(false);
+      expect(whiteRegion.contains({ x: -1, y: 0 })).toEqual(false);
+    });
+  });
+
+  describe("#merge", function () {
+    beforeEach(function () {
+      whiteRegion.merge(blackRegion);
+    });
+
+    it("combines the given region's cells with this region's", function () {
+      expect(whiteRegion.size).toEqual(24);
+    });
+
+    it("removes the given region's cells", function () {
+      expect(blackRegion.size).toEqual(0);
+    });
+
+    it("removes the merged region from the regioned image", function () {
+      expect(regionedImage.regions.length).toEqual(1);
+      expect(regionedImage.regions[0]).toEqual(whiteRegion);
+    });
   });
 });
